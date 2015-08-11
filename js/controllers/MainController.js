1e61fc655dce2b45063531c3fcd996ec0965d7d4
Sam Jewell <sam.jewell@cantab.net>
2015-08-11 07:05:05 +0100

    add conditions array

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,48 +1,37 @@
 app.controller('MainController', ['$scope', function($scope) {
-  $scope.exercises = [
+  $scope.conditions = [
     {
-      icon: 'img/pushup.jpg',
-      name: 'Pushups',
-      count: 20
+      name: 'Mental Health',
+      para1: 'Alcohol is a depressant, so it can make your moods low. Therefore it is very easy to slip into a vicious cycle of drinking to depress your feelings when actually the drink is making you feel worse.',
+      para2: 'Depression, anxiety, stress, low mood and trouble sleeping are common when people drink high levels of alcohol.',
+      icon: 'img/mental-health.jpg'
     },
     {
-      icon: 'img/squat.jpg',
-      name: 'Squats',
-      count: 15
+      name: 'Skin Problems',
+      para1: 'Skin ProblemsSkin ProblemsSkin ProblemsSkin ProblemsSkin ProblemsSkin ProblemsSkin ProblemsSkin Problems',
+      para2: 'Skin ProblemsSkin ProblemsSkin ProblemsSkin Problems',
+      icon: 'img/skin-problems.jpg'
     },
     {
-      icon: 'img/pullup.jpg',
-      name: 'Pullups',
-      count: 10
+      name: 'Hypertension',
+      para1: 'Alcohol is a depressant, so it can make your moods low. Therefore it is very easy to slip into a vicious cycle of drinking to depress your feelings when actually the drink is making you feel worse.',
+      para2: 'Depression, anxiety, stress, low mood and trouble sleeping are common when people drink high levels of alcohol.',
+      icon: 'img/hypertension.jpg'
     },
     {
-      icon: 'img/row.jpg',
-      name: 'Rows',
-      count: 10
-    },
-    {
-      icon: 'img/lunge.jpg',
-      name: 'Lunges',
-      count: 10
-    },
-    {
-      icon: 'img/stepup.jpg',
-      name: 'Step Ups',
-      count: 10
-    },
-    {
-      icon: 'img/situp.jpg',
-      name: 'Sit Ups',
-      count: 15
+      name: 'Fatty Liver',
+      para1: 'Fatty LiverFatty LiverFatty LiverFatty LiverFatty LiverFatty LiverFatty LiverFatty LiverFatty LiverFatty Liver',
+      para2: 'Fatty LiverFatty LiverFatty LiverFatty Liver',
+      icon: 'img/fatty-liver.jpg'
     }
   ];
 
 	$scope.increase = function(index) {
-		$scope.exercises[index].count += 1;
+		$scope.conditions[index].count += 1;
 	}
 	
 	$scope.decrease = function(index) {
-		$scope.exercises[index].count -= 1;
+		$scope.conditions[index].count -= 1;
 	}
     
 }]);
